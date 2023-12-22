20f053674836a8ba110016bc398f4debee2a513b
Yoan Gvs <yoangovaerts@gmail.com>
2023-12-22 10:45:14 +0100

    IW-73 mobile header version

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,17 @@
-import { Link } from 'react-router-dom'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg'; 
 
 function Header() {
-    return (
-        <nav className='Header'>
+  return (
+    <div className="Header">
+        <img src={logo} alt="Logo" className='logo'/>
+        <nav>
             <Link to="/">Accueil</Link>
             <Link to="/about">About</Link>
         </nav>
-    )
+    </div>
+  );
 }
 
-export default Header
+export default Header;
