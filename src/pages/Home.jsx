bd5b7fba318f052bf53f14ca61fec938acafa2a4
Yoan Gvs <yoangovaerts@gmail.com>
2023-12-28 21:10:03 +0100

    IW-79 footer mobile version

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react"
 
 const Home = () => {
+    const [title, setTitle] = useState('')
 
+    const handleClick = () => {
+        setTitle('Yoan')
+    }
 
     return (
         <div>
-            <h1>Bienvenu</h1>
+            <h1 onClick={() => handleClick()}>Bienvenu {title}</h1>
         </div>
     )
 }
