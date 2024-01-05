9a9ceb51338aeb7d9c73bd401440556bfa8e83b6
Yoan Gvs <yoangovaerts@gmail.com>
2024-01-05 08:40:01 +0100

    IW-76 Missing page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import About from '../pages/About'
-import Error from '../pages/Error'
+import MissingPage from '../pages/MissingPage'
 import Header from './Header'
 import Footer from './Footer'
 
@@ -11,7 +11,7 @@ const App = () => (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/*" element={<Error />} />
+            <Route path="/*" element={<MissingPage />} />
         </Routes>
         <Footer />
     </Router>
